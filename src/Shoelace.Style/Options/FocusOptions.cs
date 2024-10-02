7f938e76bf63f2716f193736ceb06f6b5bccc526
Denny09310 <k.denny2000@gmail.com>
2024-10-02 15:40:31 +0200

    fix: minor xml docs change

--- src/Shoelace.Style/Options/FocusOptions.cs
+++ src/Shoelace.Style/Options/FocusOptions.cs
@@ -2,8 +2,15 @@
 
 namespace Shoelace.Style.Options;
 
+/// <summary>
+/// Represents options for controlling focus behavior.
+/// </summary>
 public class FocusOptions
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether to prevent scrolling when the element is focused.
+    /// If set to <c>true</c>, scrolling will be prevented; otherwise, it allows scrolling. 
+    /// </summary>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? PreventScroll { get; set; }
 }
