16cab77659613b25526f93c6ca06ca2ffe0fc4eb
Denny09310 <k.denny2000@gmail.com>
2024-10-02 15:59:38 +0200

    feat: add sl-card

---
+++ src/Shoelace.Style/Components/Card/ShoelaceCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shoelace.Style.Components;
+
+/// <summary>
+/// Cards can be used to group related subjects in a container.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/card"/>
+/// </remarks>
+public partial class ShoelaceCard : ShoelaceComponentBase
+{
+    /// <summary>
+    /// The content of the card.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
