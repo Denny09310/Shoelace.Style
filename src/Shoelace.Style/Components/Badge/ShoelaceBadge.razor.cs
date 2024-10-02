00be7126b2ecab9d5e82c3ddf6e456d8a5827083
Denny09310 <k.denny2000@gmail.com>
2024-10-02 14:07:39 +0200

    feat: add sl-badge

---
+++ src/Shoelace.Style/Components/Badge/ShoelaceBadge.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shoelace.Style.Components;
+
+public partial class ShoelaceBadge : ShoelaceComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    #region Properties
+
+    /// <summary>
+    /// Draws a pill-style badge with rounded edges.
+    /// </summary>
+    [Parameter]
+    public bool Pill { get; set; }
+
+    /// <summary>
+    /// Makes the badge pulsate to draw attention.
+    /// </summary>
+    [Parameter]
+    public bool Pulse { get; set; }
+
+    /// <summary>
+    /// The badge’s theme variant.
+    /// </summary>
+    /// <remarks>
+    /// Valid values are 'primary' | 'success' | 'neutral' | 'warning' | 'danger'
+    /// </remarks>
+    [Parameter]
+    public string? Variant { get; set; }
+
+    #endregion Properties
+}
