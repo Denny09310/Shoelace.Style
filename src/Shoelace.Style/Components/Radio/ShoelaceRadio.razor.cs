0e0d24344167c6de1a91b3e7b673d481dc7c9748
Denny09310 <k.denny2000@gmail.com>
2024-10-05 11:49:52 +0200

    feat: add sl-radio

---
+++ src/Shoelace.Style/Components/Radio/ShoelaceRadio.razor.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Shoelace.Style.Options;
+
+namespace Shoelace.Style.Components;
+
+/// <summary>
+/// Radios allow the user to select a single option from a group.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/radio"/>
+/// </remarks>
+/// <typeparam name="T">The type of the radio value</typeparam>
+public partial class ShoelaceRadio<T> : ShoelaceComponentBase, IFocusable
+{
+    /// <summary>
+    /// The radio’s label.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    #region Properties
+
+    /// <summary>
+    /// Disables the radio.
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    /// <summary>
+    /// The radio’s size. When used inside a radio group,
+    /// the size will be determined by the radio group’s size so this attribute can typically be omitted.
+    /// </summary>
+    /// <remarks>
+    /// Possible values are 'small' | 'medium' | 'large'
+    /// </remarks>
+    [Parameter]
+    public string? Size { get; set; }
+
+    /// <summary>
+    /// The radio’s value. When selected, the radio group will receive this value.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public T Value { get; set; } = default!;
+
+    #endregion Properties
+
+    #region Events
+
+    /// <inheritdoc/>
+    public EventCallback OnBlur { get; set; }
+
+    /// <inheritdoc/>
+    public EventCallback OnFocus { get; set; }
+
+    #endregion Events
+
+    #region Instance Properties
+
+    /// <inheritdoc/>
+    public ValueTask BlurAsync() => Element.InvokeVoidAsync("blur");
+
+    /// <inheritdoc/>
+    public ValueTask FocusAsync(FocusOptions options) => Element.InvokeVoidAsync("focus", options);
+
+    #endregion Instance Properties
+}
