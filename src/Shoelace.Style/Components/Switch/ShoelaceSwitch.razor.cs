373bfaa8e5d97f74f72414747ad829709a763dba
Denny09310 <k.denny2000@gmail.com>
2024-10-07 16:55:48 +0200

    feat: add sl-switch

---
+++ src/Shoelace.Style/Components/Switch/ShoelaceSwitch.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Shoelace.Style.Components;
+
+/// <summary>
+/// Switches allow the user to toggle an option on or off.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/switch"/>
+/// </remarks>
+public partial class ShoelaceSwitch : ShoelaceCheckbox
+{
+}
