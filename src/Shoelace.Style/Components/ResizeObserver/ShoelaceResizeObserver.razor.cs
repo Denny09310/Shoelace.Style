2168005c8df0742e25da5fca40674cd333e5ccfc
Denny09310 <k.denny2000@gmail.com>
2024-10-07 16:48:19 +0200

    feat: add sl-resize-observer

---
+++ src/Shoelace.Style/Components/ResizeObserver/ShoelaceResizeObserver.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shoelace.Style.Events;
+
+namespace Shoelace.Style.Components;
+
+/// <summary>
+/// The Resize Observer component offers a thin, declarative interface to the ResizeObserver API.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/resize-observer"/>
+/// </remarks>
+public partial class ShoelaceResizeObserver
+{
+    /// <summary>
+    /// One or more elements to watch for resizing.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    #region Properties
+
+    /// <summary>
+    /// Disables the observer.
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    #endregion Properties
+
+    #region Events
+
+    /// <summary>
+    /// Emitted when the element is resized.
+    /// </summary>
+    [Parameter]
+    public EventCallback<ShoelaceResizeEvent> OnResize { get; set; }
+
+    #endregion Events
+
+    /// <inheritdoc/>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await AddEventListener("sl-resize", OnResize);
+        }
+    }
+}
---
+++ src/Shoelace.Style/Events/ShoelaceResizeEvent.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shoelace.Style.Events;
+
+/// <summary>
+/// Represents a single entry for a resize observation. 
+/// Contains size details about the observed element's border box and content box.
+/// </summary>
+public class ResizeObserverEntry
+{
+    /// <summary>
+    /// Gets or sets the collection of sizes representing the element's border box size.
+    /// </summary>
+    [JsonPropertyName("borderBoxSize")]
+    public IReadOnlyCollection<ResizeObserverSize> BorderBoxSize { get; set; } = [];
+
+    /// <summary>
+    /// Gets or sets the collection of sizes representing the element's content box size.
+    /// </summary>
+    [JsonPropertyName("contentBoxSize")]
+    public IReadOnlyCollection<ResizeObserverSize> ContentBoxSize { get; set; } = [];
+}
+
+/// <summary>
+/// Internal class used for handling Shoelace component resize events.
+/// Contains an array of resize observation entries.
+/// </summary>
+public class ShoelaceResizeEvent
+{
+    /// <summary>
+    /// Gets or sets the array of entries representing resize events of observed elements.
+    /// </summary>
+    public ResizeObserverEntry[] Entries { get; set; } = [];
+}
+
+/// <summary>
+/// Represents the size of an element in terms of block and inline dimensions.
+/// Block size is typically the height, and inline size is the width, depending on layout direction.
+/// </summary>
+public class ResizeObserverSize
+{
+    /// <summary>
+    /// Gets or sets the block size of the observed element (height in most writing modes).
+    /// </summary>
+    [JsonPropertyName("blockSize")]
+    public double BlockSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets the inline size of the observed element (width in most writing modes).
+    /// </summary>
+    [JsonPropertyName("inlineSize")]
+    public double InlineSize { get; set; }
+}
