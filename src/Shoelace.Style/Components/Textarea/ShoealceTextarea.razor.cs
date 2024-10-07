78f040c4ad9ee028a4ffbbdd91a451e5474ed383
Denny09310 <k.denny2000@gmail.com>
2024-10-07 17:01:54 +0200

    feat: add sl-textarea

---
+++ src/Shoelace.Style/Components/Textarea/ShoealceTextarea.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shoelace.Style.Components;
+
+/// <summary>
+/// Textareas collect data from the user and allow multiple lines of text.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/textarea"/>
+/// </remarks>
+public partial class ShoealceTextarea : ShoelaceInput<string>
+{
+    #region Properties
+
+    /// <summary>
+    /// The number of rows to display by default.
+    /// </summary>
+    [Parameter]
+    public int Rows { get; set; }
+
+    /// <summary>
+    /// Controls how the textarea can be resized.
+    /// </summary>
+    /// <remarks>
+    /// Posiible values are	'none' | 'vertical' | 'auto'
+    /// </remarks>
+    [Parameter]
+    public string Resize { get; set; }
+
+    #endregion
+}
