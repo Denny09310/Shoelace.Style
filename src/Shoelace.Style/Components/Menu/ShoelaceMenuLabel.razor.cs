59bb3b055deeb5e7e7ae22887656ab92a663670c
Denny09310 <k.denny2000@gmail.com>
2024-10-15 17:27:55 +0200

    feat: added sl-menu-label

---
+++ src/Shoelace.Style/Components/Menu/ShoelaceMenuLabel.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shoelace.Style.Components;
+
+/// <summary>
+/// Menu labels are used to describe a group of menu items.
+/// </summary>
+/// <remarks>
+/// <see href="https://shoelace.style/components/menu-label"/>
+/// </remarks>
+public partial class ShoelaceMenuLabel : ShoelaceComponentBase
+{
+    /// <summary>
+    /// The menu label’s content.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
