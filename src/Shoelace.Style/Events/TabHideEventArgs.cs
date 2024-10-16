ed79e40304c59d93633b79b2193706092c7c9cde
Denny09310 <k.denny2000@gmail.com>
2024-10-16 11:02:53 +0200

    chore: added documentation for sl-tab-show/sl-tab-hidden

--- src/Shoelace.Style/Events/TabHideEventArgs.cs
+++ src/Shoelace.Style/Events/TabHideEventArgs.cs
@@ -1,6 +1,18 @@
 ﻿namespace Shoelace.Style.Events;
 
-public class TabHideEventArgs :EventArgs
+/// <summary>
+/// Provides data for the event that occurs when a tab is hidden.
+/// </summary>
+/// <remarks>
+/// This event is typically raised when a user switches to a different tab or when the tab group programmatically hides a tab.
+/// </remarks>
+public class TabHideEventArgs : EventArgs
 {
+    /// <summary>
+    /// Gets or sets the name of the tab that was hidden.
+    /// </summary>
+    /// <value>
+    /// A string representing the name of the tab that was hidden.
+    /// </value>
     public string Name { get; set; } = default!;
 }
--- src/Shoelace.Style/Events/TabShowEventArgs.cs
+++ src/Shoelace.Style/Events/TabShowEventArgs.cs
@@ -1,6 +1,18 @@
 ﻿namespace Shoelace.Style.Events;
 
+/// <summary>
+/// Provides data for the event that occurs when a tab is shown.
+/// </summary>
+/// <remarks>
+/// This event is typically raised when a user selects a tab or when the tab group programmatically shows a tab.
+/// </remarks>
 public class TabShowEventArgs : EventArgs
 {
+    /// <summary>
+    /// Gets or sets the name of the tab that was shown.
+    /// </summary>
+    /// <value>
+    /// A string representing the name of the tab that was shown.
+    /// </value>
     public string Name { get; set; } = default!;
 }
